1fbd857319dbcec197780435cdb4a1abb8c3b5c2
tikowikoFamily <mrgamerdu84@gmail.com>
2026-08-23 21:26:50 +0200

    Active le correctif runtime de la ville nuit

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -9,6 +9,7 @@ import {
 } from "@tanstack/react-router";
 import { useEffect, type ReactNode } from "react";
 
+import "@/lib/runtime-visual-fix";
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 import { Toaster } from "@/components/ui/sonner";
